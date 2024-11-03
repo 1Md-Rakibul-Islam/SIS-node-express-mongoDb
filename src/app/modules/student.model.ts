68a697baf67e9e2f0f8b8a7e1f3ff7bff9c36b13
1Md-Rakibul-Islam <rakibul.islam.developer@gmail.com>
2024-11-03 22:11:07 +0600

    updated

--- src/app/modules/student.model.ts
+++ src/app/modules/student.model.ts
@@ -5,6 +5,7 @@ import {
     Student,
     UserName,
 } from './student/student.interface';
+import validator from 'validator';
 
 const userNameSchema = new Schema<UserName>({
     firstName: {
@@ -27,6 +28,10 @@ const userNameSchema = new Schema<UserName>({
         type: String,
         required: [true, 'Last name is required'],
         trim: true,
+        validate: {
+            validator: (value: string) => validator.isAlpha(value),
+            message: '{VALUE} is not a valid name',
+        }
     },
 });
 
@@ -93,7 +98,15 @@ const studentSchema = new Schema<Student>({
         required: [true, 'Gender is required'],
     },
     dateOfBirth: { type: String },
-    email: { type: String, required: [true, 'Email is required'] },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+        validate: {
+            validator: (value: string) => validator.isEmail(value),
+            message: '{VALUE} is not a valid email',
+        }
+    },
     contactNo: { type: String, required: [true, 'Contact number is required'], unique: true },
     emergencyContactNo: { type: String, required: [true, 'Emergency contact number is required'] },
     bloodGroup: {
